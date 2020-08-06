722bef72ade892a8bf1d325d0d962889804325e8
Mohamed Jinas <j@live.mv>
2020-08-06 07:31:01 +0500

    Update BmlOcrManager.php

--- src/BmlOcrManager.php
+++ src/BmlOcrManager.php
@@ -4,46 +4,75 @@ namespace Baraveli\BmlOcr;
 
 use Intervention\Image\ImageManagerStatic as Image;
 use thiagoalessio\TesseractOCR\TesseractOCR;
+use Baraveli\BmlOcr\Receipt;
 
 class BmlOcrManager
 {
     protected $hashedImage;
     protected $temporaryDirectory;
 
-    public function make(string $imagepath)
+    /**
+     * make
+     *
+     * @param  mixed $imagepath
+     * @param  mixed $temporaryDirectory
+     * @return BmlOcrManager
+     */
+    public function make(string $imagepath, string $temporaryDirectory): BmlOcrManager
     {
-        $this->hashedImage = md5($imagepath).'.jpg';
+        $this->temporaryDirectory = $temporaryDirectory;
+        $this->hashedImage = md5($imagepath) . '.jpg';
         $this->sharpenImage($imagepath);
 
         return $this;
     }
 
-    public function setTempDirectory(string $path)
+    /**
+     * Detect the receipt
+     *
+     * @return Receipt
+     */
+    public function detect(): Receipt
     {
-        $this->temporaryDirectory = $path;
-
-        return $this;
-    }
-
-    public function detect(): Invoice
-    {
-        $text = (new TesseractOCR($this->hashedImage))->run();
+        $text = (new TesseractOCR($this->temporaryDirectory . DIRECTORY_SEPARATOR . $this->hashedImage))->run();
         //Remove the temporary image
-        //unlink($this->hashedImage);
+        unlink($this->temporaryDirectory . DIRECTORY_SEPARATOR . $this->hashedImage);
         return $this->filter($text);
     }
 
+    /**
+     * GetHashedImage
+     *
+     * @return string
+     */
+    public function GetHashedImage(): string
+    {
+        return $this->hashedImage;
+    }
+
+    /**
+     * Sharpen the image and store as a temporary file.
+     *
+     * @param  mixed $imagepath
+     * @return void
+     */
     protected function sharpenImage(string $imagepath): void
     {
         Image::make($imagepath)
             ->sharpen(25)
-            ->save($this->temporaryDirectory ? $this->temporaryDirectory.DIRECTORY_SEPARATOR.$this->hashedImage : $this->hashedImage);
+            ->save($this->temporaryDirectory . DIRECTORY_SEPARATOR . $this->hashedImage);
     }
 
-    protected function filter(string $text): Invoice
+    /**
+     * FIlter the texts and remove any empty values.
+     *
+     * @param  mixed $text
+     * @return Receipt
+     */
+    protected function filter(string $text): Receipt
     {
         $result = array_values(array_filter(explode("\n", $text)));
 
-        return new Invoice($result);
+        return new Receipt($result);
     }
 }
